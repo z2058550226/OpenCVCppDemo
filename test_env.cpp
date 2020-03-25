9bf64106b9663571a835a4e101f94d2bab53faa2
z2058550226 <2058550226@qq.com>
2020-03-25 16:39:36 +0800

    butter mac has no video capture, use suika pc

---
+++ test_env.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat srcImage = imread("/Users/suikajy/vscode-workspace/LearnCmake/res/in_leaves.jpg");
+    if (!srcImage.data) {
+        cout << "Image not loaded";
+        return -1;
+    }
+
+    imshow("[img]", srcImage);
+    waitKey(0);
+
+    return 0;
+}
